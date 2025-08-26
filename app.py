60de295068cf7a2185f39a846b64c015e925a3d3
Exodussubhash <53942222+Exodussubhash@users.noreply.github.com>
2025-08-26 23:02:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, request ,url_for
+from google.cloud import storage
 import os
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+# app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+GCS_BUCKET_NAME = 'visualize-you'  
+
+
+def upload_to_gcs(file):
+    # Initialize GCS client
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(GCS_BUCKET_NAME)
+    blob = bucket.blob(file.filename)
+
+    # Upload the file to GCS
+    blob.upload_from_file(file)
+
+    return blob.public_url
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     output = None
+    file_url = None
     if request.method == 'POST':
         uploaded_file = request.files.get('file')
         color = request.form.get('color')
         profession = request.form.get('profession')
         mars_visualize = request.form.get('mars_visualize')
         if uploaded_file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-            uploaded_file.save(file_path)
-            file_url = url_for('static', filename='uploads/' + uploaded_file.filename)
-        if uploaded_file:
+            file_url = upload_to_gcs(uploaded_file)
             if 'change_color' in request.form:
                 output = f"Change shirt color to {color} for file: {uploaded_file.filename}"
             elif 'see_me_as' in request.form:
@@ -35,4 +47,5 @@ def index():
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
+
     app.run(debug=True)
